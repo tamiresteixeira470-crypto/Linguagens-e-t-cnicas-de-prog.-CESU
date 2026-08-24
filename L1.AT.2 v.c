f619eeef21a0394e30ea61cceff52100406eb198
Tamires <tamiresteixeirabonifacio470@gmail.com>
2026-08-24 17:56:21 -0300

    Add files via upload

---
+++ L1.AT.2 v.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	double a;
+	 
+	 printf("insira um valor:",a);
+	 
+	 
+	 scanf("%lf", &a);
+	 	printf("%e\n", a);
+	 
+
+	
+	
+	
+	
+	
+	
+	return 0;
+}
