e797ac9e0229130fc03f6445ba0d50f58dc01bd3
Tamires <tamiresteixeirabonifacio470@gmail.com>
2026-08-25 09:32:16 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+float v,r,pi;
+
+ pi=3.14159;
+scanf("%f",&r); 
+
+v=(4.0/3)*pi*r;
+
+printf("v=%.3f\n",v);
+	return 0;
+}
